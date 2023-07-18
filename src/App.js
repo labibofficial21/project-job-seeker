7db560e07395a45ee83c6f3cdef219bd0cf7316c
labibofficial21 <labibofficial21@gmail.com>
2023-07-19 06:54:32 +0700

    clean some cod

--- src/App.js
+++ src/App.js
@@ -1,48 +1,38 @@
 import React from 'react'
 import './App.css'
 
-import Buttons from './components2/atoms/Buttons'
-import InputBar from './components2/organisms/InputBar'
+import LandingPage from './components2/page/Navbar/LandingPage'
+import FindJobs from './components2/page/Navbar/FindJobs'
+import Example from './contohResponsive/Responsive'
+
+// Organism folder
+import Company from './components2/organisms/Company'
 import DropdownBar from './components2/organisms/DropdownBar'
-import Textfield from './components2/organisms/Textfield'
-import SearchBar from './components2/organisms/SearchBar'
-import TabsBar from './components2/organisms/TabsBar'
-import StepsBar from './components2/organisms/StepsBar'
-import StageBar from './components2/organisms/StageBar'
+import InputBar from './components2/organisms/InputBar'
 import JobCategory from './components2/organisms/JobCategory'
 import JobList from './components2/organisms/JobList'
-import Company from './components2/organisms/Company'
-import LandingPage from './components2/page/Navbar/LandingPage'
+import SearchBar from './components2/organisms/SearchBar'
+import StageBar from './components2/organisms/StageBar'
+import StepsBar from './components2/organisms/StepsBar'
+import TabsBar from './components2/organisms/TabsBar'
+import Textfield from './components2/organisms/Textfield'
+
+// Atoms folder
+import Buttons from './components2/atoms/Buttons'
+
 import Homepage from './components2/page/Navbar/Homepage'
-import Layout from 'antd/es/layout/layout'
 import LayoutPage from './components2/page/Navbar/LayoutPage'
 import ContohSaja from './components2/page/Navbar/ContohSaja'
-import Example from './contohResponsive/Responsive'
-import FindJobs from './components2/page/Navbar/FindJobs'
 
 
 export default function App() {
 	return (
 		<div className='container' >
-			{/* <LandingPage /> */}
-			<FindJobs />
+			<LandingPage />
+			{/* <FindJobs /> */}
 
-
+			{/* Penggunaan useMediaQuery */}
 			{/* <Example /> */}
-			{/* <ContohSaja /> */}
-			{/* <Homepage /> */}
-			{/* <LayoutPage /> */}
-			{/* <Company /> */}
-			{/* <JobList /> */}
-			{/* <JobCategory /> */}
-			{/* <StageBar /> */}
-			{/* <StepsBar /> */}
-			{/* <TabsBar /> */}
-			{/* <SearchBar /> */}
-			{/* <Buttons /> */}
-			{/* <InputBar /> */}
-			{/* <DropdownBar /> */}
-			{/* <Textfield /> */}
 		</div>
 	)
 }
